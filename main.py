6c6e58e4a6b7dc41cc03778f7880835f18635610
KatarzynaMariaElzbieta <kasia.gajc@wp.pl>
2023-07-23 13:38:56 +0200

    poetry

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 from const_config import FONTS
-from app.dashboard_app.views.main_layout import create_main_layout
+from dashboard_app.views.main_layout import create_main_layout
 
 app = dash.Dash(__name__, use_pages=True, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP,
@@ -11,4 +11,4 @@ app = dash.Dash(__name__, use_pages=True, suppress_callback_exceptions=True,
 app.layout = create_main_layout(dash.page_registry, dash.page_container)
 
 if __name__ == "__main__":
-    app.run_server(debug=True, dev_tools_props_check=False)
+    app.run_server(debug=True, dev_tools_props_check=False, host='0.0.0.0', port='5000')
